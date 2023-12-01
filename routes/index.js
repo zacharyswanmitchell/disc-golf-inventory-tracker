adefde3dbcef10fcec3d66f4ff29a89bb3aa91f9
Zachary Mitchell <masspike84@yahoo.com>
2023-12-01 14:55:14 -0500

    modify login redirect to bags

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect: '/',
+    successRedirect: '/bags',
     failureRedirect: '/'
   }
 ));
